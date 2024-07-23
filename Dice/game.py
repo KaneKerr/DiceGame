4fe8e5e409cb37956c2edd50a75bd1229cc40323
Kane Kerr <121164278+KaneKerr@users.noreply.github.com>
2024-07-24 00:53:39 +0100

    Multiplier addition added
    Allows for dynamic multiplier, the lower the win chance the higher the multiplier.

--- Dice/game.py
+++ Dice/game.py
@@ -5,10 +5,9 @@ new_game = True
 amount = 0
 my_list = []
 rolled_number = 0
-multiplier = 9900
+multiplier = 0
 chosen_number = int(input("Enter bet number you wish for it to roll higher than: "))
-bet_size = 0
-
+start_num = 0
 
 for i in range(1, 100):
     my_list.append(i)
@@ -16,7 +15,7 @@ for i in range(1, 100):
 
 
 def add_win_to_account():
-    bet_size = float(input("Enetr bet size lot: "))
+    bet_size = float(input("Eneter bet size lot: "))
     global amount
     if amount == 0:
         print("Deposit into account")
@@ -25,6 +24,15 @@ def add_win_to_account():
     elif rolled_number > chosen_number:
         amount = bet_size * multiplier + amount
     return amount
+
+
+def run_game():
+    global multiplier
+    global start_num
+    while start_num < chosen_number:
+        start_num += 1
+        multiplier += 5
+    return multiplier
 
 
 def set_amount():
@@ -36,8 +44,9 @@ if new_game:
     set_amount()
 
 
-
-print(rolled_number)
+run_game_edit = run_game()
+print("Your multiplier is: " + str(run_game_edit))
+print("the number rolled was: " + str(rolled_number))
 new_balance = add_win_to_account()
 print("New balance is: " + str(new_balance))
 
