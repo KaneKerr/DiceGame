8d0f9f664a90beafb47849375e7acce30ce785d2
Kane Kerr <121164278+KaneKerr@users.noreply.github.com>
2024-07-25 20:35:55 +0100

    Added range checks for varies properties
    Added range checks for the chosen number inputted by user
    Added checks to see if a user wants to start a new game
    Rearranged output so that it makes more sense from a users perspective

--- Dice/game.py
+++ Dice/game.py
@@ -1,32 +1,43 @@
 import random
 
 
-new_game = True
+new_game = False
 amount = 0
-my_list = []
-rolled_number = 0
+my_list = list(range(1, 100))
+rolled_number = random.choice(my_list)
 multiplier = 0
-chosen_number = int(input("Enter bet number you wish for it to roll higher than: "))
 start_num = 0
 
-for i in range(1, 100):
-    my_list.append(i)
-    rolled_number = random.choice(my_list)
+start_again = (input("Type 'Yes/yes' if you want to start again: "))
+
+
+if start_again == "Yes" or start_again == "yes":
+    new_game = True
+
+
+def get_valid_number():
+    while True:
+        chosen_number2 = int(input("Enter bet number you wish for it to roll higher than between the numbers 1 and 99: "))
+        if chosen_number2 >= my_list[0] and chosen_number2 <= my_list[98]:
+            return chosen_number2
+        else:
+            print("Invalid number. Choose between 1 and 99.")
+
+
+chosen_number = get_valid_number()
 
 
 def add_win_to_account():
-    bet_size = float(input("Eneter bet size lot: "))
     global amount
-    if amount == 0:
-        print("Deposit into account")
-    elif bet_size > amount:
+    bet_size = float(input("Enter bet size lot: "))
+    if bet_size > amount:
         print("Account size too small for bet")
     elif rolled_number > chosen_number:
         amount = bet_size * multiplier + amount
     return amount
 
 
-def run_game():
+def add_multi():
     global multiplier
     global start_num
     while start_num < chosen_number:
@@ -36,19 +47,22 @@ def run_game():
 
 
 def set_amount():
-    global amount
-    amount = int(input("Enter Account size: "))
+    while True:
+        global amount
+        amount = float(input("Enter Account size: "))
+        if amount == 0:
+            print("Deposit into account")
+        else:
+            return amount
 
 
 if new_game:
     set_amount()
 
-
-run_game_edit = run_game()
+run_game_edit = add_multi()
 print("Your multiplier is: " + str(run_game_edit))
-print("the number rolled was: " + str(rolled_number))
 new_balance = add_win_to_account()
+print("the number rolled was: " + str(rolled_number))
 print("New balance is: " + str(new_balance))
-
 
 
